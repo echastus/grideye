fa5d562b1802d957c565020da8296a9a477287c4
Bartek Pietraś <bpietras@Barteks-MacBook-Air.local>
2021-12-14 19:43:22 +0100

    Added a new main.py file and made some completely unsignificant changes to serialRead.py.

---
+++ main.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+import cv2 as cv
+
+from serial import Serial
+
+
+def to_csv(file):
+    with open(file, "r") as f:
+        for line in f:
+            if len(line) > 1:
+                values = np.array(line.split(" ")[:-1])
+                values.resize((8, 8))
+                print(values)
+
+
+def main():
+    # to_csv("sensorOutput")
+    # port_name = '/dev/cu.usbmodem14101'
+    with open("sensorOutput", "r") as f:
+        lines = [np.array(line.split(" ")[:-1]).reshape((8, 8)) for line in f if len(line) > 1]
+        for line in lines:
+            print(line)
+
+
+if __name__ == "__main__":
+    main()
--- serialRead.py
+++ serialRead.py
@@ -2,23 +2,25 @@ import serial
 import numpy as np
 import cv2 as cv
 
-def translate(frame): # maps a range of degrees (init) to 8-bit colour values (result)
-    initMin = int(frame[-1]) #last element of frame is value of potentiometer already in degrees
+
+def translate(frame):  # maps a range of degrees (init) to 8-bit colour values (result)
+    initMin = int(frame[-1])  # last element of frame is value of potentiometer already in degrees
     initMax = 60
     resultMin = 0
     resultMax = 254
 
     initSpan = initMax - initMin
     resultSpan = resultMax - resultMin
-    
+
     for i in range(64):
         value = float(frame[i])
-        newValue = max(0, float(value - initMin) / float(initSpan)) # max(0, x) doesnt allow newValue to be negative
+        newValue = max(0.0, float(value - initMin) / float(initSpan))  # max(0, x) doesnt allow newValue to be negative
         frame[i] = np.uint8(resultMin + (newValue * resultSpan))
-        
+
+
 def frameToImage(frame):
     sideSize = 64
-    image = np.zeros((sideSize*8, sideSize*8, 3), np.uint8)         
+    image = np.zeros((sideSize * 8, sideSize * 8, 3), np.uint8)
 
     translate(frame)
 
@@ -26,31 +28,31 @@ def frameToImage(frame):
         y = t // 8
         x = t % 8
 
-        for p in range(y*sideSize, (y+1)*sideSize):
-            for r in range(x*sideSize, (x+1)*sideSize):
-                image[r, p, 1] = frame[t] # sadly, full rbg colours slows the imshow() function
+        for p in range(y * sideSize, (y + 1) * sideSize):
+            for r in range(x * sideSize, (x + 1) * sideSize):
+                image[r, p, 1] = frame[t]  # sadly, full rbg colours slows the imshow() function
 
     return image
 
+
 # main
+unoPort1 = 'COM7'
+unoPort2 = '/dev/cu.usbmodem14101'
+serialport = serial.Serial(unoPort2, baudrate=115200, timeout=2)
 
-unoPort = 'COM7'
-serialport = serial.Serial(unoPort, baudrate = 115200, timeout = 2)
-output = open('sensorOutput', 'a')
+with open('sensorOutput', 'a'):
 
-while(1):
-    try:
-        data = serialport.readline().decode('ascii')
-        frame = data.split(" ")
+    while True:
+        try:
+            data = serialport.readline().decode('ascii')
+            frame = data.split(" ")
 
-        if len(frame) == 65: 
-            # output.write(data)
-            cv.imshow('grideye output', frameToImage(frame))
-            k = cv.waitKey(1) & 0xFF
-            if k == ord('q'):
-                break
+            if len(frame) == 65:
+                # output.write(data)
+                cv.imshow('grideye output', frameToImage(frame))
+                k = cv.waitKey(1) & 0xFF
+                if k == ord('q'):
+                    break
 
-    except UnicodeDecodeError: # mistake in serial stream encoding?
-        continue
-
-output.close()
+        except UnicodeDecodeError:  # mistake in serial stream encoding?
+            continue
