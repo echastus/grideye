40f4883753ddd6d8f18ae3f776cc125ba936ffe3
Bartek Pietraś <bpietras@Barteks-MacBook-Air.local>
2021-12-14 21:28:13 +0100

    Added a basic functionality for scaling and filtering the unwanted values of temperature and a simple visualisation for debugging, that is kind of similiar to the one from serialRead.py file.

--- main.py
+++ main.py
@@ -15,11 +15,22 @@ def to_csv(file):
 
 
 def main():
-    # to_csv("sensorOutput")
-    # port_name = '/dev/cu.usbmodem14101'
     with open("sensorOutput", "r") as f:
-        lines = [np.array(line.split(" ")[:-1]).reshape((8, 8)) for line in f if len(line) > 1]
-        print("GITHUB CHANGE")
+        frames = [np.array(line.split(" ")[:-1]).reshape((8, 8)) for line in f if len(line) > 1]
+        for frame in frames:
+            frame = np.transpose(frame).astype(np.float32)
+            new_frame = np.zeros((frame.shape[0] * 64, frame.shape[1] * 64, 3), dtype=np.uint8)
+            for i, row in enumerate(frame):
+                for j, column in enumerate(row):
+                    temp = np.uint8(column / 100 * 255)
+                    if temp > 60:
+                        new_frame[(64 * i):(64 * i) + 64, (64 * j):(64 * j) + 64, 1] = temp
+                    else:
+                        new_frame[(64 * i):(64 * i) + 64, (64 * j):(64 * j) + 64, 1] = 0
+            cv.imshow("frame", new_frame)
+            cv.waitKey(50)
+
+    cv.destroyAllWindows()
 
 
 if __name__ == "__main__":
