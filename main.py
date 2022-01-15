37f94f766776b082c8b1d624a6566c3c458a9637
Bartek Pietraś <bpietras@Barteks-MacBook-Air.local>
2022-01-15 13:09:47 +0100

    Quick commit, cause my laptop charger might be dead by the time of the next coding session.

--- main.py
+++ main.py
@@ -20,15 +20,15 @@ def str_to_arr(frame):
     return frame
 
 
-# def arr_to_img(frame):
-#     new_frame = np.zeros(shape=(8 * 64, 8 * 64, 3), dtype=np.uint8)
-#     for i, row in enumerate(frame):
-#         for j, column in enumerate(row):
-#             new_frame[(64 * i):(64 * i) + 64, (64 * j):(64 * j) + 64, :] = frame[i, j] / 80 * 255
-#     return new_frame
-
-
 def arr_to_img(frame):
+    new_frame = np.zeros(shape=(8 * 64, 8 * 64, 3), dtype=np.uint8)
+    for i, row in enumerate(frame):
+        for j, column in enumerate(row):
+            new_frame[(64 * i):(64 * i) + 64, (64 * j):(64 * j) + 64, :] = frame[i, j] / 80 * 255
+    return new_frame
+
+
+def arr_to_img2(frame):
     new_frame = np.zeros(shape=(8, 8, 3), dtype=np.uint8)
     for i, row in enumerate(frame):
         for j, column in enumerate(row):
@@ -50,22 +50,30 @@ def display_from_file(filename):
 
 
 def detect_motion(filename):
+    # Initialising necessary variables.
     df = pd.read_csv(filename, header=None)
     fgbg = cv.createBackgroundSubtractorMOG2()
+    bools = np.zeros(shape=(3, ), dtype=bool)
+    # Loop that goes through each frame
     for i in range(len(df)):
-        bools = np.zeros(shape=(8,), dtype=bool)
         frame = np.array(df.iloc[i]).reshape((8, 8))
-        frame = arr_to_img(np.transpose(frame))
+        frame2 = arr_to_img(np.transpose(frame))
+        frame = arr_to_img2(np.transpose(frame))
         fgmask = fgbg.apply(frame)
-        for k in range(8):
-            temp = 0
-            for i in range(8):
-                temp += fgmask[i][k]
-            if temp >= (255 + 255 + 255):
-                bools[k] = True
-        print(fgmask)
-        print(bools)
+        temp = sum_halves(fgmask)
+        if temp[1] < temp[0]:
+            bools[0] = True
+        if temp[1] > temp[0]:
+            bools[1] = True
+        if bools[1] and bools[0] and temp[1] < 765:
+            bools[2] = True
 
+        if all(bools):
+            print("Someone just went from left to right!")
+            bools.fill(False)
+        cv.imshow("Frame", frame2)
+        cv.waitKey(100)
+        cv.destroyAllWindows()
 
 
 def display_from_port():
@@ -108,6 +116,31 @@ def write_to_file(video_length, num_of_vids, sleep_time=5.5):
     os.remove('trashfile.csv')
 
 
+def sum_halves(x):
+    temp1 = 0
+    temp2 = 0
+    for i in range(8):
+        for j in range(8):
+            if j < 4:
+                temp1 += x[i][j]
+            else:
+                temp2 += x[i][j]
+
+    return tuple((temp1, temp2))
+
+
+def left_to_right(frame, bools):
+    temp = sum_halves(frame)
+    if temp[1] < temp[0]:
+        bools[0] = True
+    if temp[1] > temp[0]:
+        bools[1] = True
+    if bools[1] and bools[0]:
+        bools[2] = True
+
+    return bools
+
+
 if __name__ == "__main__":
     # display_from_file('program_output5.csv')
     # display_from_port()
@@ -115,4 +148,44 @@ if __name__ == "__main__":
     # main_algorithm_with_no_specified_name_for_the_time_being()
     detect_motion('program_output5.csv')
 
-
+    # x0 = np.zeros(shape=(8, 8), dtype=np.uint8)
+    # x1 = np.zeros(shape=(8, 8), dtype=np.uint8)
+    # x1[0][0] = 255
+    # x1[1][0] = 255
+    # x1[1][1] = 255
+    # x1[2][1] = 255
+    # x1[2][2] = 255
+    # x1[3][2] = 255
+    #
+    # x2 = np.zeros(shape=(8, 8), dtype=np.uint8)
+    # x2[7][7] = 255
+    # x2[7][6] = 255
+    # x2[6][6] = 255
+    # x2[5][6] = 255
+    # x2[4][5] = 255
+    # x3 = np.zeros(shape=(8, 8), dtype=np.uint8)
+    # #765
+    # print(x1)
+    # print(x2)
+    # bools = np.zeros(shape=(3, ), dtype=bool)
+    # temps0 = sum_halves(x0)
+    # temps1 = sum_halves(x1)
+    # temps2 = sum_halves(x2)
+    # temps3 = sum_halves(x3)
+    # print(temps0)
+    # print(temps1)
+    # print(temps2)
+    # print(temps3)
+    #
+    # temps = [temps0, temps1, temps2, temps3]
+    #
+    # for temp in temps:
+    #     if temp[0] >= 765 and temp[1] < temp[0]:
+    #         bools[0] = True
+    #     if temp[1] > temp[0] and temp[1] >= 765:
+    #         bools[1] = True
+    #     if bools[1] and bools[0] and temp[1] < 765:
+    #         bools[2] = True
+    #
+    # if all(bools):
+    #     print("Someone just went from left to right")
