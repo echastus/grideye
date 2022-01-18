493af7c97066f7de939ee851dbfed12954f44b1a
Bartek Pietraś <bpietras@Barteks-MacBook-Air.local>
2022-01-18 14:57:33 +0100

    Got the algorithm working on all 'videos', but one
    It seems that the temperature readings might be slightly off, because the object was moving really fast. That bug might be caused by the Grid-EYE hardware limitations, but I am going to investigate it later.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import NaturalLanguage
 import numpy as np
 import pandas as pd
 import cv2 as cv
@@ -27,7 +28,7 @@ def arr_to_img(frame):
     new_frame = np.zeros(shape=(8 * 64, 8 * 64, 3), dtype=np.uint8)
     for i, row in enumerate(frame):
         for j, column in enumerate(row):
-            new_frame[(64 * i):(64 * i) + 64, (64 * j):(64 * j) + 64, :] = frame[i, j] / 80 * 255
+            new_frame[(64 * i):(64 * i) + 64, (64 * j):(64 * j) + 64, :] = frame[i, j] / 100 * 255
     return new_frame
 
 
@@ -35,7 +36,7 @@ def arr_to_img2(frame):
     new_frame = np.zeros(shape=(8, 8, 3), dtype=np.uint8)
     for i, row in enumerate(frame):
         for j, column in enumerate(row):
-            new_frame[i, j, :] = frame[i, j] / 80 * 255
+            new_frame[i, j, :] = frame[i, j] / 100 * 255
     return new_frame
 
 
@@ -52,12 +53,13 @@ def display_from_file(filename):
     cv.destroyAllWindows()
 
 
-def main(filename):
+def main(filename, debug=False):
     # Initialising necessary variables.
+    framerate = 50
     df = pd.read_csv(filename, header=None)
     fgbg = cv.createBackgroundSubtractorMOG2()
-    left_to_right = np.zeros(shape=(3, ), dtype=bool)
-    right_to_left = np.zeros(shape=(3, ), dtype=bool)
+    ltr = np.zeros(shape=(3, ), dtype=bool)
+    rtl = np.zeros(shape=(3, ), dtype=bool)
     # Loop that goes through each frame.
     for i in range(len(df)):
         frame = np.array(df.iloc[i]).reshape((8, 8))
@@ -67,44 +69,69 @@ def main(filename):
         # Create an image for the algorithm to work on.
         temp_frame = arr_to_img2(np.transpose(frame))
         fgmask = fgbg.apply(temp_frame)
+        for x, row in enumerate(fgmask):
+            for y, column in enumerate(row):
+                if fgmask[x][y] > 0:
+                    fgmask[x][y] = 255
         frame2 = arr_to_img(fgmask)
 
         # Brain of the algorithm.
 
         # Returns the sum of pixel values of the left half and the right half of the image.
-        pixel_halves = sum_pixel_vals(fgmask)
+        pixels = sum_pixel_vals(fgmask)
         temps = sum_temps(frame)
-        print(temps)
-        print(np.abs(temps[0] - temps[1]))
+        pixels_th = 765
+        temps_th = 620
 
         # Checking all the conditions for people going left to right.
-        if pixel_halves[1] < pixel_halves[0] and pixel_halves[0] > 765 and not right_to_left[0]:
-            left_to_right[0] = True
-            right_to_left.fill(False)
-        if pixel_halves[1] > pixel_halves[0] and left_to_right[0] and not right_to_left[0]:
-            left_to_right[1] = True
-        if left_to_right[1] and left_to_right[0] and pixel_halves[1] < 765 and not right_to_left[0]:
-            left_to_right[2] = True
+        if pixels[1] < pixels[0] and pixels[0] > pixels_th and not rtl[0]:
+            if temps[1] < temps[0] and temps[0] > temps_th:
+                ltr[0] = True
+                rtl.fill(False)
+        if pixels[1] > pixels[0] and ltr[0] and not rtl[0]:
+            if temps[1] > temps[0]:
+                ltr[1] = True
+                rtl.fill(False)
+        if ltr[1] and ltr[0] and pixels[1] < pixels_th and not rtl[0]:
+            if temps[1] < temps_th:
+                ltr[2] = True
+            elif temps[0] > temps_th:
+                ltr[1] = False
 
         # Checking all the conditions for people going right to left.
-        if pixel_halves[0] < pixel_halves[1] and pixel_halves[1] > 765 and not left_to_right[0]:
-            right_to_left[0] = True
-            left_to_right.fill(False)
-        if pixel_halves[0] > pixel_halves[1] and right_to_left[0] and not left_to_right[0]:
-            right_to_left[1] = True
-        if right_to_left[1] and right_to_left[0] and pixel_halves[0] < 765 and not left_to_right[0]:
-            right_to_left[2] = True
+        if pixels[0] < pixels[1] and pixels[1] > pixels_th and not ltr[0]:
+            if temps[0] < temps[1] and temps[1] > temps_th:
+                rtl[0] = True
+                ltr.fill(False)
+        if pixels[0] > pixels[1] and rtl[0] and not ltr[0]:
+            if temps[0] > temps[1]:
+                ltr.fill(False)
+                rtl[1] = True
+        if rtl[1] and rtl[0] and pixels[0] < pixels_th and not ltr[0]:
+            if temps[0] < temps_th:
+                rtl[2] = True
+            elif temps[1] > temps_th:
+                rtl[1] = False
+
+        if debug:
+            print(f'pixels[0], temps[0]: {pixels[0]}, {temps[0]}')
+            print(f'pixels[1], temps[1]: {pixels[1]}, {temps[1]}')
+            print(f'Left to right: {ltr}')
+            print(f'Right to left: {rtl}\n')
+            framerate = 1
 
         # Displaying information
-        if all(left_to_right):
+        if all(ltr):
             print("Someone has just entered the room!")
-            left_to_right.fill(False)
-        if all(right_to_left):
+            ltr.fill(False)
+            rtl.fill(False)
+        if all(rtl):
             print("Someone has just left the room!")
-            right_to_left.fill(False)
+            rtl.fill(False)
+            ltr.fill(False)
         cv.imshow("Frame", frame2)
         cv.imshow("Frame2", frame3)
-        cv.waitKey(80)
+        cv.waitKey(framerate)
     cv.destroyAllWindows()
 
 
@@ -150,11 +177,7 @@ def write_to_file(video_length, num_of_vids, sleep_time=5.5):
         time.sleep(sleep_time)
     os.remove('trashfile.csv')
 
-def read_and_write_port():
-    serial_connection = Serial(arduino_port1, baudrate=115200, timeout=2)
 
-
-<<<<<<< HEAD
 def sum_pixel_vals(x):
     left = 0
     right = 0
@@ -165,13 +188,6 @@ def sum_pixel_vals(x):
             else:
                 right += x[i][j]
     return tuple((left, right))
-=======
-if __name__ == "__main__":
-    # display_from_file('program_output3.csv')
-    display_from_port()
-    # write_to_file(10, 3)
-    # main_algorithm_with_no_specified_name_for_the_time_being()
->>>>>>> 9a82be4a4b4604c0bf7cfd121f8f5281806ce560
 
 
 def sum_temps(x):
@@ -187,4 +203,5 @@ def sum_temps(x):
 
 
 if __name__ == "__main__":
-    main('program_output2.csv')
+    main('program_output5.csv', debug=True)
+# 5, 8
